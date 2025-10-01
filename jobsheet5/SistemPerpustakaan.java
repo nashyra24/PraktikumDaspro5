a9db31476d4060e11ed6d2aa54622c037f1a807b
nashyra24 <140595105+nashyra24@users.noreply.github.com>
2025-10-01 10:43:22 +0700

    Tugas 3

---
+++ jobsheet5/SistemPerpustakaan.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SistemPerpustakaan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Sistem Perpustakaan ---");
+
+        // Input kartu mahasiswa
+        System.out.print("Apakah memiliki kartu mahasiswa? (true/false): ");
+        boolean kartuMahasiswa = sc.nextBoolean();
+
+        // Input registrasi online
+        System.out.print("Apakah sudah registrasi online? (true/false): ");
+        boolean registrasiOnline = sc.nextBoolean();
+
+        // Proses pengecekan sesuai flowchart
+        if (kartuMahasiswa || registrasiOnline) {
+            System.out.println("Boleh Masuk");
+        } else {
+            System.out.println("Ditolak Masuk");
+        }
+
+        sc.close();
+    }
+}
