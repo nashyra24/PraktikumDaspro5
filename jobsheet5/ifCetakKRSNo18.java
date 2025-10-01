eb75f830e94e273ad074d712151c7150ea9ef5d3
nashyra24 <140595105+nashyra24@users.noreply.github.com>
2025-10-01 10:04:42 +0700

    Tugas1

---
+++ jobsheet5/ifCetakKRSNo18.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ifCetakKRSNo18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+        String hasil = (uktLunas) 
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(hasil);
+        sc.close();
+    }
+}
