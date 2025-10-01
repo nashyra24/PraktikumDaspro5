43fdca7805c4ff20d0c0e03cfbe77d876c01adf5
nashyra24 <140595105+nashyra24@users.noreply.github.com>
2025-10-01 09:34:39 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS19.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class ifCetakKRS19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+            System.out.println("UKT belum lunas, silakan lakukan pembayaran terlebih dahulu");
+        }
+
+        sc.close();
+    }
+}
